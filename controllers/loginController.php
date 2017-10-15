7a4b17304c15e4554a1356a3749a46ef18ffad29
Ernandes Xeo <ernandesxeo@gmail.com>
2017-10-15 20:04:09 -0200

    login 1

--- controllers/loginController.php
+++ controllers/loginController.php
@@ -3,6 +3,9 @@
 include_once '../models/login.php';
 include_once '../models/usuario.php';
 
+
+
+
 // início de sessão php
 if (!isset($_SESSION)) {
     session_start();
@@ -13,16 +16,19 @@ if ($_REQUEST['botao'])
 
 // Instancia classe Login
 $login = new Login();
+
 switch ($botao) {
     case 'Entrar':
         // verificação se o login nao está vazio
         $usuario = new Usuario();
-        if (!empty($_POST['user'])) {
+        if (!empty($_POST['user'])) 
             $usuario->setNome($_POST['user']);
-        }
+        
         // verifica se a senha nao está vazia
         if (!empty($_POST['senha']))
             $usuario->setSenha(md5($_POST['senha']));
+		
+		
 
 
 
